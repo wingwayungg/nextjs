63f3cff23ca0e875b3494ca39d8ecfa236213988
YUNG WING WA <>
2022-09-29 22:02:24 -0700

    test

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"></img>
             <main className={styles.main}>
                 <h1 className={styles.title}>
                     Welcome to <a href="https://nextjs.org">Next.js!</a>
